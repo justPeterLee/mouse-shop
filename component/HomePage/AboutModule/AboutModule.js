ed7d6043300ccaf12944eb4f825558e76746333c
Peter Lee <PeterLee2094@gmail.com>
2023-02-13 21:22:30 -0600

    finished template

--- component/HomePage/AboutModule/AboutModule.js
+++ component/HomePage/AboutModule/AboutModule.js
@@ -2,7 +2,8 @@ import styles from "./AboutModule.module.css";
 export default function AboutModule() {
   return (
     <div className={styles.container}>
-      <h1>About</h1>
+      <h1 className={styles.titleAbout}>About</h1>
+      <button className={styles.button}> learn more </button>
     </div>
   );
 }
