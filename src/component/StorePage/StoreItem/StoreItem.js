4a844ba9ec5234447740a306eb4704650df0c07d
Peter Lee <PeterLee2094@gmail.com>
2023-03-31 22:56:12 -0500

    added store item with styling

---
+++ src/component/StorePage/StoreItem/StoreItem.js
@@ -0,0 +1,19 @@
+import styles from "./StoreItem.module.css";
+
+export default function StoreItem() {
+  return (
+    <div className={styles.body}>
+
+      <div className={styles.image}></div>
+
+      <div className={styles.desc}>
+        <div>
+          <p className={styles.title}>Item Name</p>
+          <p className={styles.cat}>mouse</p>
+        </div>
+        <p className={styles.price}>$25</p>
+      </div>
+
+    </div>
+  );
+}
--- src/component/StorePage/StoreMain.js
+++ src/component/StorePage/StoreMain.js
@@ -1,10 +1,17 @@
 import styles from './StoreMain.module.css';
+import StoreItem from './StoreItem/StoreItem';
 export default function StoreMain(){
     return(
         <div className={styles.container}>
             <div className={styles.filterContainer}></div>
 
-            <div className={styles.itemContainer}></div>
+            <div className={styles.itemContainer}>
+                <StoreItem/>
+                <StoreItem/>
+                <StoreItem/>
+                <StoreItem/>
+                <StoreItem/>
+            </div>
         </div>
     )
 }
