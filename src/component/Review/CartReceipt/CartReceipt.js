abd3eaa52ad1257d7d081af24a65af520215e127
Peter Lee <PeterLee2094@gmail.com>
2023-04-04 15:18:08 -0500

    added Review Cart Files added alone with Cart Receipt

---
+++ src/component/Review/CartReceipt/CartReceipt.js
@@ -0,0 +1,6 @@
+import styles from './CartReceipt.module.css';
+export default function CartReceipt(){
+    return(
+        <div></div>
+    )
+}
---
+++ src/component/Review/reviewIndex/Review.js
@@ -0,0 +1,10 @@
+import styles from './Review.module.css';
+import CartReceipt from '../CartReceipt/CartReceipt';
+
+export default function Review(){
+    return(
+        <div>
+            <CartReceipt/>
+        </div>
+    )
+}
