32a14c75320fd9789734abfecb92763e1b4b5e0d
Peter Lee <PeterLee2094@gmail.com>
2023-04-10 17:14:55 -0500

    added fetch filter products saga into redux

--- src/redux/sagas/main.store.saga.js
+++ src/redux/sagas/main.store.saga.js
@@ -3,12 +3,12 @@ import { put, takeEvery } from 'redux-saga/effects';
 // watcher sage
 function* mainStoreSaga() {
   yield takeEvery("FETCH_MAIN_PRODUCTS", fetchMainProducts);
+  yield takeEvery("FETCH_FILTER_PRODUCT", fetchFilterProducts);
 }
 
 // fetch all products
 function* fetchMainProducts() {
   try {
-    console.log('workin')
     const response = yield fetch("/api/routes/store", {
       method: "GET",
       headers: {
@@ -25,5 +25,13 @@ function* fetchMainProducts() {
   }
 }
 
+// fetch filter products
+function* fetchFilterProducts(){
+  try{
+
+  }catch(err){
+    console.log('Error with getting filterd products: ', err);
+  }
+}
 
 export default mainStoreSaga;
