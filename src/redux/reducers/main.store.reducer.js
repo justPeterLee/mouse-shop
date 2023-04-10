fed046376b938b807973be01c42721a44552ef6e
Peter Lee <PeterLee2094@gmail.com>
2023-04-10 17:11:54 -0500

    added filter store reducer to redux

--- src/redux/reducers/main.store.reducer.js
+++ src/redux/reducers/main.store.reducer.js
@@ -9,19 +9,32 @@ Main Store Reducer
 --------------------
 */
 
-function mainStore(state = [], action){
-    switch(action.type){
-        case 'SET_MAIN_STORE':
-            return state = action.payload;
-        case 'RESET_MAIN_STORE':
-            return state = [];
-        case 'RESET_STORE':
-            return state =[];
-        default:
-            return state = [];
-    }
+function mainStore(state = [], action) {
+  switch (action.type) {
+    case "SET_MAIN_STORE":
+      return (state = action.payload);
+    case "RESET_MAIN_STORE":
+      return (state = []);
+    case "RESET_STORE":
+      return (state = []);
+    default:
+      return (state = []);
+  }
 }
 
+function filterStore(state = [], action) {
+  switch (action.type) {
+    case "SET_FILTER_STORE":
+      return (state = action.payload);
+    case "REST_FILTER_STORE":
+      return (state = []);
+    case "RESET_STORE":
+      return (state = []);
+    default:
+      return (state = []);
+  }
+}
 export default combineReducers({
-    mainStore,
-})
+  mainStore,
+  filterStore,
+});
