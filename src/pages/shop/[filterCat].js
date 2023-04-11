1ed736393cad4ec3edf6755d668245b6a5a44933
Peter Lee <PeterLee2094@gmail.com>
2023-04-10 19:02:54 -0500

    added filter cat prop

--- src/pages/shop/[filterCat].js
+++ src/pages/shop/[filterCat].js
@@ -1,11 +1,10 @@
-import StoreMain from "@/component/StorePage/StoreMain";
 import StoreMenu from "@/component/StoreMenu/StoreMenu";
 import StoreFilter from "@/component/StorePage/StoreFilter/StoreFilter";
 export default function ShopAllPage(){
     return(
         <div className="top-module" styles={{display:'flex', justifyContent: 'center', gap: '2rem'}}>
             <StoreMenu/>
-            <StoreFilter/>
+            <StoreFilter catId={2}/>
         </div>
     )
 }
