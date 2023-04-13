fdb546ce626708002050fb9bd0640b31af23f788
Peter Lee <PeterLee2094@gmail.com>
2023-04-13 18:04:39 -0500

    created getStaticPaths and fixed bugs

--- src/pages/products/[productId].js
+++ src/pages/products/[productId].js
@@ -1,9 +1,42 @@
+import prisma from "lib/db";
+
 import ProductPage from "@/component/Product/index/ProductPage";
 
-export default function products(){
+export default function products(props){
     return(
         <div>
             <ProductPage/>
         </div>
     )
+}
+
+// getStaticProps (created getStaticProps to render data)
+export async function getStaticProps(context){
+    // declare the id of the product
+    const {params} = context;
+
+    // connect to the database
+    await prisma.$connect;
+
+    
+    return{
+        props:{
+            test: 'test'
+        }
+    }
+}
+
+// getStaticPaths (created paths from dynamic routes)
+export async function getStaticPaths(){
+    
+    // connect to the database
+    await prisma.$connect;
+
+
+    return {
+        paths:[
+            {params: {productId: '1'}}
+        ],
+        fallback: 'blocking'
+    }
 }
